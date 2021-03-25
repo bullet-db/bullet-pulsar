5fddb5dee53e8eb552b1972bae4298bbb7871014
Akshay Sarma <akshaisarma@gmail.com>
2021-03-25 11:38:14 -0700

    Core 1.2.1 (#8)

--- src/test/java/com/yahoo/bullet/pulsar/PulsarConfigTest.java
+++ src/test/java/com/yahoo/bullet/pulsar/PulsarConfigTest.java
@@ -13,7 +13,7 @@ public class PulsarConfigTest {
     @Test
     public void testDefaults() {
         PulsarConfig pulsarConfig = new PulsarConfig();
-        Assert.assertEquals(pulsarConfig.get(PulsarConfig.PULSAR_MAX_UNCOMMITTED_MESSAGES), 50L);
+        Assert.assertEquals(((Number) pulsarConfig.get(PulsarConfig.PULSAR_MAX_UNCOMMITTED_MESSAGES)).intValue(), 50);
         Assert.assertEquals(pulsarConfig.get(PulsarConfig.PULSAR_AUTH_ENABLE), false);
         Assert.assertNull(pulsarConfig.get("dne"));
     }
@@ -21,7 +21,7 @@ public class PulsarConfigTest {
     @Test
     public void testLoadFromFile() {
         PulsarConfig pulsarConfig = new PulsarConfig("test_config.yaml");
-        Assert.assertEquals(pulsarConfig.get(PulsarConfig.PULSAR_MAX_UNCOMMITTED_MESSAGES), 100L);
+        Assert.assertEquals(((Number) pulsarConfig.get(PulsarConfig.PULSAR_MAX_UNCOMMITTED_MESSAGES)).intValue(), 100);
         Assert.assertNull(pulsarConfig.get("dne"));
     }
 
@@ -29,12 +29,12 @@ public class PulsarConfigTest {
     public void testCopyConfig() {
         BulletConfig config = new BulletConfig();
         config.set(PulsarConfig.PULSAR_MAX_UNCOMMITTED_MESSAGES, 100L);
-        Assert.assertEquals(config.get(PulsarConfig.PULSAR_MAX_UNCOMMITTED_MESSAGES), 100L);
+        Assert.assertEquals(((Number) config.get(PulsarConfig.PULSAR_MAX_UNCOMMITTED_MESSAGES)).intValue(), 100);
         Assert.assertNull(config.get(PulsarConfig.PULSAR_AUTH_ENABLE));
         Assert.assertNull(config.get("dne"));
 
         PulsarConfig pulsarConfig = new PulsarConfig(config);
-        Assert.assertEquals(pulsarConfig.get(PulsarConfig.PULSAR_MAX_UNCOMMITTED_MESSAGES), 100L);
+        Assert.assertEquals(((Number) config.get(PulsarConfig.PULSAR_MAX_UNCOMMITTED_MESSAGES)).intValue(), 100);
         Assert.assertEquals(pulsarConfig.get(PulsarConfig.PULSAR_AUTH_ENABLE), false);
         Assert.assertNull(pulsarConfig.get("dne"));
     }
